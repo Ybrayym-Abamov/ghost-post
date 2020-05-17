6a01902d04cff2a0c541dc153bdcddfd290cda56
Ybrayym <ybrayymjan90@gmail.com>
2020-05-16 23:02:04 -0400

    add a READEME.md file, references

---
+++ README.md
@@ -0,0 +1,1 @@
+ The GhostPost Machine™ is a website where people can anonymously post Boasts or Roasts of whatever they want. Like most services, there is a character limit: 280 characters.
--- ghostpost/form.py
+++ ghostpost/form.py
@@ -7,3 +7,7 @@ RoastOrBoast = ((True, 'Boast'), (False, 'Roast'))
 class GhostPost(forms.Form):
     boolean = forms.ChoiceField(choices=RoastOrBoast)
     body = forms.CharField(widget=forms.Textarea(attrs={'placeholder': "Let the world know what's up"}))
+
+
+# https://www.geeksforgeeks.org/choicefield-django-forms/
+# http://www.learningaboutelectronics.com/Articles/How-to-add-a-placeholder-to-a-Django-form-field.php
